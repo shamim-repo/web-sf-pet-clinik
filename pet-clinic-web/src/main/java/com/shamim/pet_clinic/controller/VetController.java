66887ccce3ab73427782ca7e44cbff22737ef56f
Abdullah Bhuiyan Shamim <msbshamim60@gmail.com>
2021-03-12 14:06:41 +0600

    index and Controller.

---
+++ pet-clinic-web/src/main/java/com/shamim/pet_clinic/controller/VetController.java
@@ -0,0 +1,15 @@
+package com.shamim.pet_clinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String vetList(){
+        return "vets/index";
+    }
+}
