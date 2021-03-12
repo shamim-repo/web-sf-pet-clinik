dbca5cb5b8f3b2683d1f64d0753733802b605a77
Abdullah Bhuiyan Shamim <msbshamim60@gmail.com>
2021-03-12 15:21:01 +0600

    Show Owner and Vet Data in index Id Generation.

--- pet-clinic-data/src/main/java/com/shamim/pet_clinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/shamim/pet_clinic/service/map/AbstractMapService.java
@@ -1,20 +1,27 @@
 package com.shamim.pet_clinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.shamim.pet_clinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID,T> map=new HashMap<>();
+import java.util.*;
+
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long,T> map=new HashMap<>();
+
     Set<T> findAll(){
         return new HashSet(map.values());
     }
     T findById(ID id){
         return map.get(id);
     }
-    T save(ID id, T object){
-         map.put(id,object);
+    T save(T object){
+
+        if(object!=null){
+            if(object.getId()==null)
+                object.setId(getNextId());
+            map.put(object.getId(),object);
+        }else
+            throw new RuntimeException("Object cannot be Null");
         return object;
     }
 
@@ -23,5 +30,14 @@ public abstract class AbstractMapService<T, ID> {
     }
     void delete(T object){
         map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+    private Long getNextId(){
+        Long nextId=null;
+        try {
+            nextId=Collections.max(map.keySet())+1;
+        }catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/com/shamim/pet_clinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/shamim/pet_clinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1=new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Shamim");
         owner1.setLastName("Bhuiyan");
 
         ownerService.save(owner1);
 
         Owner owner2=new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Sharif");
         owner2.setLastName("Chaudhory");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading Owner......");
 
         Vet vet1=new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Abid");
         vet1.setLastName("Bhuiyan");
 
         vetService.save(vet1);
 
         Vet vet2=new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Tamim");
         vet2.setLastName("Copy");
 
