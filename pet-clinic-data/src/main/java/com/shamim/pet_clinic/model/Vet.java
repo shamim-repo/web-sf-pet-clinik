6cd152136ca1f82c38c786e0664a08523b18a524
Abdullah Bhuiyan Shamim <msbshamim60@gmail.com>
2021-03-16 14:37:25 +0600

    Pet and Visit Entity Setup.

--- pet-clinic-data/src/main/java/com/shamim/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/shamim/pet_clinic/model/Vet.java
@@ -5,6 +5,7 @@
  */
 package com.shamim.pet_clinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +13,12 @@ import java.util.Set;
  *
  * @author Root
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id")
+                                           inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities =new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
--- pet-clinic-data/src/main/java/com/shamim/pet_clinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/shamim/pet_clinic/model/Visit.java
@@ -1,10 +1,16 @@
 package com.shamim.pet_clinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToMany
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
